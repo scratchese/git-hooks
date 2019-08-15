d4de6339776851e953f6f8726e70e16d9ff3ba27
Andy Chen <andycandomore@gmail.com>
2019-08-14 23:20:17 -0700

    update

--- install.js
+++ install.js
@@ -3,14 +3,13 @@ const path = require('path')
 const { exec } = require('child_process')
 const cwd = process.cwd()
 const pkgJsonScript = require(path.join(cwd, 'package.json'))['scripts']
-const precommitPath = path.join('.git', 'hooks', 'pre-commit');
+const precommitPath = path.join('.git', 'hooks', 'pre-commit')
 // const prepushPath = path.join('.git', 'hooks', 'pre-push')
 
 exec(`rm -rf ${precommitPath}`, () => {
   exec(`touch ${precommitPath}`, () => {
-    exec(`echo "#!/bin/sh" > ${precommitPath}`, ()=>{
-      exec(`echo ${pkgJsonScript['precommit']} >> ${precommitPath}`);
+    exec(`echo "#!/bin/sh" > ${precommitPath}`, () => {
+      exec(`echo ${pkgJsonScript['precommit']} >> ${precommitPath}`)
     });
-  })
-})
-
+  });
+});
