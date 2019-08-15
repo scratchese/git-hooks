71b0f4eed837aa1487c439930159aa424adec7a3
amazingandyyy <andycandomore@gmail.com>
2019-08-15 08:47:48 -0700

    Update install.js

--- install.js
+++ install.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const { exec, spawn } = require('child_process')
+const { exec } = require('child_process')
 
 function installHook (action) {
   const source = path.join(__dirname, 'hooks', `${action}.hook`)
   const target = path.join('.git', 'hooks', action)
   exec(`cp -f ${source} ${target}`, () => {
     exec(`chmod ${target}`)
-  });
+  })
 };
 
 installHook('pre-commit')
