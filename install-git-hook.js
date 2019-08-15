2dd2771ccc06ea84ff8406410352c918da0a69c8
amazingandyyy <andycandomore@gmail.com>
2019-08-15 07:35:03 -0700

    Update install-git-hook.js

--- install-git-hook.js
+++ install-git-hook.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const { exec } = require('child_process')
+const { exec, spawn } = require('child_process')
 
-function installHook(action) {
-    const source = path.join(__dirname, 'hooks', `${action}.hook`);
-    const target = path.join('.git', 'hooks', action);
-    exec(`cp -f ${source} ${target}`, () => {
-        exec(`chmod ${target}`)
-    })
+function installHook (action) {
+  const source = path.join(__dirname, 'hooks', `${action}.hook`)
+  const target = path.join('.git', 'hooks', action)
+  exec(`cp -f ${source} ${target}`, () => {
+    exec(`chmod ${target}`)
+  })
 }
 
 installHook('pre-commit')
